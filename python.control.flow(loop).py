3f65ef138d61ba6cc77557a1809607726c4f45fd
ayush jivani <ayushjivani082@gmail.com>
2026-08-21 20:57:48 +0530

    Add files via upload

---
+++ python.control.flow(loop).py
@@ -0,0 +1,36 @@
+
+i = 1
+
+while i <= 5:
+    print(f"while : {i}")
+    i += 1
+
+
+
+for i in range(0 , 7):
+    print(f"for loop : {i}")
+
+for i in range(1 , 6):
+    print(f"for loop : {i}")
+
+name = "animal"
+
+for ch in name:
+    print(ch)
+
+fruits = ["Apple" , "Banana" , "Mango" , "Orange"]
+num = [1 , 2 , 3 , 4 , 5 , 6]
+
+print(fruits[0])
+print(fruits[1])
+
+for fruit in fruits:
+    print(fruit)
+
+for number in num:
+    print(number * 19)
+    
+
+
+
+      
