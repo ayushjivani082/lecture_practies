ee2efbf721ef6736301634b7efbebad01ec6b94e
ayush jivani <ayushjivani082@gmail.com>
2026-09-01 10:29:20 +0530

    Refactor and clean up Python pattern printing code

--- practies.python.patterns.py
+++ practies.python.patterns.py
@@ -1,12 +1,5 @@
 # python patterns
 
-n = 5
-
-for i in range(1, n + 1):
-    print(" " * (2 * (n - i)) + "  ".join("*" * i))
-
-for i in range(n - 1, 0, -1):
-    print(" " * (2 * (n - i)) + "  ".join("*" * i))
 
 
 # star Triangle
