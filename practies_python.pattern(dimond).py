1a65bd8bbf8487f934c7284c5eca0e941b6153c3
ayush jivani <ayushjivani082@gmail.com>
2026-09-01 10:38:18 +0530

    Add files via upload

---
+++ practies_python.pattern(dimond).py
@@ -0,0 +1,16 @@
+
+# pattern
+
+rows = int(input("Enter number of rows:"))
+for i in range(1 , rows+1):
+    for j in range(rows-i):
+        print(" ",end=" ")
+    for j in range(2*i-1):
+        print("*",end=" ")
+    print()
+for i in range(rows-1,0,-1):
+    for j in range(rows-i):
+        print(" ",end=" ")
+    for j in range(2*i-1):
+        print("*",end=" ")
+    print()
