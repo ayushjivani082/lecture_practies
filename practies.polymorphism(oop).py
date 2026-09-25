f6c8c3a0135e9774b69ee313a42ddf43f8168ae1
ayush jivani <ayushjivani082@gmail.com>
2026-09-25 13:44:49 +0530

    Add files via upload

--- practies.polymorphism(oop).py
+++ practies.polymorphism(oop).py
@@ -82,3 +82,64 @@ f.work()
 companys = [Developer() , Employee() , Designer()]
 for company in companys:
     company.work()
+
+
+# issubclass()
+
+print(issubclass(Employee , Developer))
+
+print(issubclass(Dog , Animal))
+
+print(issubclass(Animal , Dog))
+
+
+# super()
+
+class Person:
+
+    def __init__(self , name):
+        self.name = name
+
+class Student(Person):
+
+    def __init__(self , name , marks):
+        super().__init__(name)
+        self.marks = marks
+
+s = Student("Raj" , 68)
+
+p = Person("Ayush")
+
+print(s.name)
+print(s.marks)
+print(p.name)
+
+class Employee:
+
+    def __init__(self , name , salary):
+        self.name = name
+        self.salary = salary
+
+    def display(self):
+        print(f"Nmae : {self.name}")
+        print(f"Salary : {self.salary}")
+
+class Developer(Employee):
+
+    def __init__(self ,name , salary , language):
+        super().__init__(name , salary)
+        self.language = language
+
+    def display(self):
+        super().display()
+        print(f"Language : {self.language}")
+
+d = Developer("Ayush" , 56000 , "English")
+
+print(d.name)
+print(d.salary)
+print(d.language)
+
+
+d.display()
+    
